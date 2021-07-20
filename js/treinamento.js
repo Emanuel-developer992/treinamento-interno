8a1ae83646bab3f62b3767b7ad95b130916aa23c
Emanuel Ferreira <Orion_Developer@outlook.com>
2021-07-20 09:21:20 -0300

    first commit

---
+++ js/treinamento.js
@@ -0,0 +1,40 @@
+//ABRIR MODAL
+
+//Janela 1
+function open_w1 () {
+
+    $("#W1").removeClass("nav-close");
+}
+
+//Janela 2
+function open_w2 () {
+
+    $("#W2").removeClass("nav-close");
+}
+
+//FECHAR MODAL
+
+//Janela 1
+function exit_w1() {
+
+    $("#W1").addClass("nav-close");
+
+}
+
+//Janela 2
+function exit_w2() {
+
+    $("#W2").addClass("nav-close");
+
+}
+
+//NAVEGAÇÃO TESTE
+function c_breve() {
+
+    $("#W-EB").removeClass("nav-close");
+}
+
+function exit_breve() {
+
+    $("#W-EB").addClass("nav-close");
+}
